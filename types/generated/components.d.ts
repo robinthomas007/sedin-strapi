b5efd99659e3a5c425d29b8a0f67ab8bb78464a3
railsfactory-robin <robinthomas@railsfactory.org>
2024-06-17 10:52:13 +0530

    demo landing page api changes

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,73 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ElementsButtonLink extends Schema.Component {
+  collectionName: 'components_elements_button_links';
+  info: {
+    displayName: 'Button Link';
+  };
+  attributes: {
+    label: Attribute.String;
+    type: Attribute.Enumeration<['BTN-PRIMARY', 'BTN-SECONDARY']>;
+  };
+}
+
+export interface ElementsCard extends Schema.Component {
+  collectionName: 'components_elements_cards';
+  info: {
+    displayName: 'Card';
+  };
+  attributes: {
+    title: Attribute.Text;
+    description: Attribute.Text;
+    cardimg: Attribute.Media;
+    cardbtn: Attribute.Component<'elements.button-link'>;
+  };
+}
+
+export interface ElementsSlider extends Schema.Component {
+  collectionName: 'components_heading_sliders';
+  info: {
+    displayName: 'Slider';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.Text;
+    description: Attribute.Text;
+    actionButton: Attribute.Component<'elements.button-link'>;
+    sliderimage: Attribute.Media;
+  };
+}
+
+export interface RowCardRow extends Schema.Component {
+  collectionName: 'components_row_card_rows';
+  info: {
+    displayName: 'cardRow';
+  };
+  attributes: {
+    title: Attribute.String;
+    cards: Attribute.Component<'elements.card', true>;
+  };
+}
+
+export interface RowRow extends Schema.Component {
+  collectionName: 'components_row_rows';
+  info: {
+    displayName: 'row';
+  };
+  attributes: {
+    Headersliders: Attribute.String;
+    sliders: Attribute.Component<'elements.slider', true>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'elements.button-link': ElementsButtonLink;
+      'elements.card': ElementsCard;
+      'elements.slider': ElementsSlider;
+      'row.card-row': RowCardRow;
+      'row.row': RowRow;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,41 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiLandingPageLandingPage extends Schema.CollectionType {
+  collectionName: 'landing_pages';
+  info: {
+    singularName: 'landing-page';
+    pluralName: 'landing-pages';
+    displayName: 'Landing Page';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    slug: Attribute.UID<'api::landing-page.landing-page', 'title'>;
+    headerSliders: Attribute.DynamicZone<['row.row']>;
+    BetterTheBest: Attribute.DynamicZone<['row.card-row']>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::landing-page.landing-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::landing-page.landing-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -798,6 +833,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::landing-page.landing-page': ApiLandingPageLandingPage;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
